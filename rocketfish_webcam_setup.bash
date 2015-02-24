c0f11c5d23667cd1b26581086bf8bd5b7d991d0f
emdelaney <delaney15@up.edu>
2015-02-23 17:29:52 -0800

    Adding a setup file for the rocketfish webcam.

--- rocketfish_webcam_setup.bash
+++ rocketfish_webcam_setup.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script sets up the usb_cam parameters such that the module
+# works with the Rocketfish AF webcam.
 rosparam set usb_cam/framerate 15
 rosparam set usb_cam/image_height 600
 rosparam set usb_cam/image_width 800
