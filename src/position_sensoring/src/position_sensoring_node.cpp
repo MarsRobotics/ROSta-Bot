badeddb6f70882f9efc023324dc4d7bf90b0d2cb
viki <liao15@up.edu>
2015-02-26 17:49:43 -0800

    Cleaned up main position_sensoring file

--- src/position_sensoring/src/position_sensoring_node.cpp
+++ src/position_sensoring/src/position_sensoring_node.cpp
@@ -24,25 +24,20 @@ int inputKey = 0;
 
 //Path to files
 string boardConfigFile;
-
 string cameraConfigFile;
 
 VideoCapture videoCapture;
-
 Mat inputImage,inputImageCopy;
 BoardConfiguration boardConfig;
 BoardDetector boardDetector;
 CameraParameters cameraParams;
-Mat rvec, tvec;				// for storing translation and rotation matrices
+Mat rvec, tvec;		// for storing translation and rotation matrices
 
 int boardHeight = 500;
 int boardWidth = 500;
 Vector<Marker> markers;
 
-
-
-
-
+// Copied from ArUco library
 void getObjectAndImagePoints(Board &B, vector<cv::Point3f> &objPoints,vector<cv::Point2f> &imagePoints) {
     //composes the matrices
     int nPoints=B.size()*4;
@@ -57,144 +52,108 @@ void getObjectAndImagePoints(Board &B, vector<cv::Point3f> &objPoints,vector<cv:
     }
 }
 
-int main(int argc,char **argv)
-{
-    try
-    {
-        //if (argc!=3) {
-        //    cerr<<"Usage: boardConfig.yml camera_config.yml "<<endl;
-        //    return -1;
-        //}
-
-        //Get the camera config
-       	//cameraConfigFile = argv[2];
-       	cameraParams.readFromXMLFile("/home/viki/ROSta-Bot/src/position_sensoring/src/cam_param.yml");
-
-        //Read the board
-        //boardConfigFile = argv[1];
-        boardConfig.readFromFile("/home/viki/ROSta-Bot/src/position_sensoring/src/single_marker.yml");
-
-        //Open the camera
-
-        videoCapture.open(0);
-//        videoCapture.open("/home/mars/Dropbox/MarsFiles/marker.avi");
-
-        //make sure camera is open
-        if (!videoCapture.isOpened()) {
-            cerr<<"Could not open the video"<<endl;
-            return -1;
-        }
-
-        //read an image from camera
-        videoCapture >> inputImage;
-	//cameraParams.resize(inputImage);
-
-        //create the gui
-        cv::namedWindow("output video", CV_WINDOW_AUTOSIZE);
-
-        boardDetector.setParams(boardConfig, cameraParams);
-
-	vector<Point3d> boardPoints;
-
-
-	//generate vectors for the points on the chessboard
-	for (int i=0; i<boardWidth; i++)
-	{
-		for (int j=0; j<boardHeight; j++)
-		{
-			boardPoints.push_back( Point3d( double(i), double(j), 0.0) );
+int main(int argc,char **argv) {
+	try {
+		if (argc != 3) {
+			cerr << "Usage: boardConfig.yml camera_config.yml " << endl;
+			return -1;
 		}
-	}
+
+		//Read the board configuration file
+		boardConfigFile = argv[1];
+		boardConfig.readFromFile(boardConfigFile);
+		//IE: boardConfig.readFromFile("/home/viki/ROSta-Bot/src/position_sensoring/src/single_marker.yml");
 
 
+		//Get the camera configurations
+		cameraConfigFile = argv[2];
+		cameraParams.readFromXMLFile(cameraConfigFile);
+		// IE: cameraParams.readFromXMLFile("/home/viki/ROSta-Bot/src/position_sensoring/src/cam_param.yml");
 
-        while ( inputKey != 27 && videoCapture.grab()) {
+		//Open the camera
+		videoCapture.open(0);
 
-            //retrieve an image from the camera
-            videoCapture.retrieve(inputImage);
-            inputImage.copyTo(inputImageCopy);
-
-
-            boardDetector.detect(inputImage);
-            markers = boardDetector.getDetectedMarkers();
-            for (unsigned int i = 0; i < markers.size(); i++) {
-                Marker m = markers[i];
-		/*
-                float camData[] = {1.3089596458327883e+03, 0.0, 3.1716402612677911e+02, 0.0, 1.3249652390726415e+03, 2.3359932814285278e+02, 0., 0., 1.0};
-                Mat cam = Mat(3, 3, CV_32F, camData);
-                float distortionData[] = {1.8869609562810794e+00, 4.2431486029577115e+01,
-                        -7.9002380674102313e-02, -1.0615309141897006e-02,
-                        -1.2940788383601671e+03};
-                Mat distortion = Mat(1, 5, CV_32F, distortionData);
-                Size size = Size(640, 480);
-
-                CameraParameters cp = CameraParameters(cam, distortion, size);
-
-//                This isn't needed I guess
-//                m.calculateExtrinsics(MARKER_SIZE, cp);
-*/ 
-                Point topLeft = m[0];
-                Point topRight = m[1];
-                Point bottomRight = m[2];
-                Point bottomLeft = m[3];
-             
-		vector<Point2f> corners;
-
-                //Display data to the meatbag humans
-
-                for (int i=0;i<4;i++){
-                    cout<<"("<<m[i].x<< ","<<m[i].y<<") ";
-		    Point2f a(m[i].x, m[i].y);
-		    corners.push_back(a);
+		//make sure camera is open
+		if (!videoCapture.isOpened()) {
+			cerr << "Could not open the video" << endl;
+			return -1;
 		}
-                cout << endl;
-		
 
+		//read an image from camera
+		videoCapture >> inputImage;
 
-    		cv::Mat CamMatrix=cv::Mat::eye(3,3,CV_32F);
-    		CamMatrix.at<float>(0,0)=500;
-    		CamMatrix.at<float>(1,1)=500;
-    		CamMatrix.at<float>(0,2)=inputImageCopy.size().width/2;
-    		CamMatrix.at<float>(1,2)=inputImageCopy.size().height/2;
-    		cameraParams.setParams(CamMatrix,cv::Mat::zeros(1,5,CV_32F) ,inputImageCopy.size());
+		//create the gui
+		cv::namedWindow("output video", CV_WINDOW_AUTOSIZE);
 
-             	vector<cv::Point3f> objPoints;
-		vector<cv::Point2f> imgPoints;
-                getObjectAndImagePoints(boardDetector.getDetectedBoard(),objPoints,imgPoints);
+		boardDetector.setParams(boardConfig, cameraParams);
 
+		while (inputKey != 27 && videoCapture.grab()) {
+			//retrieve an image from the camera
+			videoCapture.retrieve(inputImage);
 
-		solvePnP(objPoints, imgPoints, cameraParams.CameraMatrix, cameraParams.Distorsion, rvec, tvec);
+			// save a copy of it somewhere, in case our calculations make changes
+			inputImage.copyTo(inputImageCopy);
+			boardDetector.detect(inputImage);
 
-		cv::Mat R;
-		cv::Rodrigues(rvec, R);
-		cv::Mat cameraRotationVector;
-		cv::Rodrigues(R.t(), cameraRotationVector);
+			// detect the markers, and for each marker, determine pose and position
+			markers = boardDetector.getDetectedMarkers();
+			for (unsigned int i = 0; i < markers.size(); i++) {
+				Marker m = markers[i];
 
-		cv::Mat cameraTranslationVector = -R.t()*tvec;
+				//Display data to the meatbag humans
+				for (int i = 0; i < 4; i++) {
+					cout << "(" << m[i].x << "," << m[i].y << ") ";
+					Point2f a(m[i].x, m[i].y);
+				}
+				cout << endl;
 
-		cout << "Camera position " << cameraTranslationVector.at<double>(0) << ", " << cameraTranslationVector.at<double>(1) << ", " << cameraTranslationVector.at<double>(2) << endl;
- 
-cout << "Camera pose " << cameraRotationVector.at<double>(0) << ", " << cameraRotationVector.at<double>(1) << ", " << cameraRotationVector.at<double>(2) << endl;
- 
+				// brute force way to set up the camera matrix and parameters...may need to calibrate using ArUco
+				// if we do decide to use a calibrated parameter, then get ride of these lines...
+				cv::Mat CamMatrix = cv::Mat::eye(3, 3, CV_32F);
+				CamMatrix.at<float>(0, 0) = 500;
+				CamMatrix.at<float>(1, 1) = 500;
+				CamMatrix.at<float>(0, 2) = inputImageCopy.size().width / 2;
+				CamMatrix.at<float>(1, 2) = inputImageCopy.size().height / 2;
+				cameraParams.setParams(CamMatrix, cv::Mat::zeros(1, 5, CV_32F), inputImageCopy.size());
 
+				vector <cv::Point3f> objPoints;
+				vector <cv::Point2f> imgPoints;
 
-                m.draw(inputImageCopy, COLOR, LINE_WIDTH);
+				// get "3d" points of the object in real space, get "2d" points of the object in a flat image
+				getObjectAndImagePoints(boardDetector.getDetectedBoard(), objPoints, imgPoints);
 
-            }
+				// solve for transition matrix and store in rvec
+				// solve for rotation matrix and store in tvec
+				solvePnP(objPoints, imgPoints, cameraParams.CameraMatrix, cameraParams.Distorsion, rvec, tvec);
 
-            cv::imshow("output video",inputImageCopy);
+				// determine the position of the "camera" --> which in our case, will be the robot as well
+				cv::Mat R;
+				cv::Rodrigues(rvec, R);
+				cv::Mat cameraRotationVector;
+				cv::Rodrigues(R.t(), cameraRotationVector);
+				cv::Mat cameraTranslationVector = -R.t() * tvec;
 
+				// print the translation vectors (distance between marker and camera in x, y, z direction)
+				cout << "Camera position " << cameraTranslationVector.at<double>(0) << ", " << cameraTranslationVector.at<double>(1) << ", " << cameraTranslationVector.at<double>(2) << endl;
 
-            //Check if the stop button has been pressed
-            inputKey = cv::waitKey(WAIT_TIME);
-        }
+				// print the pose vectors (orientation between marker and camera in x,y,z direction -- angle of rotation for each)
+				cout << "Camera pose " << cameraRotationVector.at<double>(0) << ", " << cameraRotationVector.at<double>(1) << ", " << cameraRotationVector.at<double>(2) << endl;
 
-        return 0;
+				m.draw(inputImageCopy, COLOR, LINE_WIDTH);
+			}
+			// update the frame!
+			cv::imshow("output video", inputImageCopy);
 
-    } catch (std::exception &ex)
-    {
-        cout<<"Exception :"<<ex.what()<<endl;
-    }
-}
+			//Check if the stop (ESC) button has been pressed
+			inputKey = cv::waitKey(WAIT_TIME);
+		}
+
+		return 0;
+	} // end try
+	catch (std::exception &ex) {
+		cout << "Exception :" << ex.what() << endl;
+	} // end catch
+
+} // end main
 
 
