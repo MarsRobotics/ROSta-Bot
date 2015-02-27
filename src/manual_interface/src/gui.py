634692c55d193087a59d89295e6168fa22026a7f
viki <chunm18@up.edu>
2015-02-26 18:23:18 -0800

    direction and kill buttons added

--- src/manual_interface/src/gui.py
+++ src/manual_interface/src/gui.py
@@ -3,36 +3,63 @@ import sys
 from PyQt4 import QtGui
 
 #pass in base class for all UI objects, window
-class Button(QtGui.QWidget):
+class GUI(QtGui.QWidget):
     def __init__(self):
         #returns parent object
-        super(Button, self).__init__()
+        super(GUI, self).__init__()
 
         self.initUI()
 
     def initUI(self):
         #set font
         QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+        #set window width
+        w = 550
+        h = 450
 
-        #create tooltip
-        self.setToolTip('This is a <b>QWidget</b> widget')
-
-        #create button widget and set tooltip
-        btn = QtGui.QPushButton('Button', self)
-        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        #kill button
+        btn = QtGui.QPushButton('KILL', self)
+        btn.setToolTip('<b>Kill Power</b>')
         #resize button with recommended size
         btn.resize(btn.sizeHint())
-        btn.move(50, 50)
+        btn.move(w-150, h-60)
+
+        #forward button
+        btnFor = QtGui.QPushButton('Forward', self)
+        btnFor.setToolTip('<b>Move forward</b>')
+        #resize button with recommended size
+        btnFor.resize(btn.sizeHint())
+        btnFor.move(w-150, h-190)
+
+        #backward button
+        btnBack = QtGui.QPushButton('Backward', self)
+        btnBack.setToolTip('<b>Move backward</b>')
+        #resize button with recommended size
+        btnBack.resize(btn.sizeHint())
+        btnBack.move(w-150, h-120)
+
+        #right turn button
+        btnRgt = QtGui.QPushButton('Right', self)
+        btnRgt.setToolTip('<b>Move right</b>')
+        #resize button with recommended size
+        btnRgt.resize(btn.sizeHint())
+        btnRgt.move(w-100, h-155)
+
+        #left turn button
+        btnLft = QtGui.QPushButton('Left', self)
+        btnLft.setToolTip('<b>Move left</b>')
+        #resize button with recommended size
+        btnLft.resize(btn.sizeHint())
+        btnLft.move(w-200, h-155)
 
         #locates window on screen and sets size
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Hello World')
-        self.setWindowIcon(QtGui.QIcon('web.png'))
+        self.setGeometry(300, 300, w, h)
+        self.setWindowTitle('Hello Meow')
         self.show()
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    butt = Button()
+    goo = GUI()
     sys.exit(app.exec_())
 
 
