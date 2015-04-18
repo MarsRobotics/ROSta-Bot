e8f3e49593b69dbb1982e01745f98cc79f46c99c
Taylor Nightingale <nighting47@gmail.com>
2015-04-18 14:24:07 -0700

    Merged code: Cleaned up code, and added some starter functionality for handling two markers. Also changed the aruco run script to work with the new position_sensoring node.

--- src/position_sensoring/src/position_sensoring_node.cpp
+++ src/position_sensoring/src/position_sensoring_node.cpp
@@ -14,28 +14,6 @@ using namespace cv;
 using namespace aruco;
 using namespace position_sensoring;
 
-////Delay before next image retrieval
-//const int WAIT_TIME = 2;
-
-////The size of the marker in meters
-//const float MARKER_SIZE = .16;
-//const float DISTANCE_CONSTANT = 7183.16666666666666;
-
-//Path to files
-string boardConfigFile;
-string cameraConfigFile;
-
-VideoCapture videoCapture;
-Mat inputImage,inputImageCopy;
-BoardConfiguration boardConfig;
-BoardDetector boardDetector;
-CameraParameters cameraParams;
-Mat rvec, tvec;		// for storing translation and rotation matrices
-
-//int boardHeight = 500;
-//int boardWidth = 500;
-Vector<Marker> markers;
-
 int currentCameraAngle = -1;
 int targetAngle = -1;
 
@@ -62,41 +40,47 @@ void current_camera_angle_callback(const std_msgs::Int32::ConstPtr& message) {
 	currentCameraAngle = message->data;
 }
 
-
 int main(int argc,char **argv) {
-	ros::init(argc, argv, "position_sensor");
-  	ros::NodeHandle n;
-
-   	// publish to topic "target_distance", hold 1000 of these message in the buffer before discarding
-	ros::Publisher positionPub = n.advertise<position_sensoring::position>("range_data", 1000);
-	position_sensoring::position positionMsg;
-
-	// publish to the topic "marker_visible" hold 1000 of these messages in the buffer before discarding
-	ros::Publisher markerPub = n.advertise<position_sensoring::marker_visible>("is_marker_visible", 1000);
-	position_sensoring::marker_visible markerMsg;
-
-	// publish new target angle to Arduino
-	ros::Publisher updateTargetAngle = n.advertise<std_msgs::Int32>("target_camera_angle", 1000);
-
-	// listen to current angle of camera (relative to motor)
-	ros::Subscriber updateCurrentCameraAngle = n.subscribe("current_camera_angle", 1, current_camera_angle_callback);
-  	
 	try {
-		if (argc != 3) {
-			cerr << "Usage: boardConfig.yml camera_config.yml " << endl;
+		if (argc != 4) {
+			cerr << "Usage: boardConfigLarge.yml boardConfigSmall.yml camera_config.yml " << endl;
 			return -1;
 		}
 
-		//Read the board configuration file
-		boardConfigFile = argv[1];
-		boardConfig.readFromFile(boardConfigFile);
-		//IE: boardConfig.readFromFile("/home/viki/ROSta-Bot/src/position_sensoring/src/single_marker.yml");
+		ros::init(argc, argv, "position_sensor");
+		ros::NodeHandle n;
 
+		// publish to topic "target_distance", hold 1000 of these message in the buffer before discarding
+		ros::Publisher positionPub = n.advertise<position_sensoring::position>("range_data", 1000);
+		position_sensoring::position positionMsg;
+
+		// publish to the topic "marker_visible" hold 1000 of these messages in the buffer before discarding
+		ros::Publisher markerPub = n.advertise<position_sensoring::marker_visible>("is_marker_visible", 1000);
+		position_sensoring::marker_visible markerMsg;
+
+		// publish new target angle to Arduino
+		ros::Publisher updateTargetAngle = n.advertise<std_msgs::Int32>("target_camera_angle", 1000);
+
+		// listen to current angle of camera (relative to motor)
+		ros::Subscriber updateCurrentCameraAngle = n.subscribe("current_camera_angle", 1, current_camera_angle_callback);
+
+
+		// Variables to hold our boards,and configurations.
+		BoardConfiguration largeBoardConfig, smallBoardConfig;
+		BoardDetector largeBoardDetector, smallBoardDetector;
+		CameraParameters cameraParams;
+		
+		// Read the large board configuration file
+		largeBoardConfig.readFromFile(argv[1]);
+
+		// Read the small board configuration file
+		smallBoardConfig.readFromFile(argv[2]);
 
 		//Get the camera configurations
-		cameraConfigFile = argv[2];
-		cameraParams.readFromXMLFile(cameraConfigFile);
-		// IE: cameraParams.readFromXMLFile("/home/viki/ROSta-Bot/src/position_sensoring/src/cam_param.yml");
+		cameraParams.readFromXMLFile(argv[3]);
+
+		VideoCapture videoCapture;
+		cv::Mat inputImage;
 
 		//Open the camera
 		videoCapture.open(0);
@@ -107,47 +91,46 @@ int main(int argc,char **argv) {
 			return -1;
 		}
 
+		Vector<Marker> markers;
+
 		//read an image from camera
 		videoCapture >> inputImage;
 
-		boardDetector.setParams(boardConfig, cameraParams);
+		largeBoardDetector.setParams(largeBoardConfig, cameraParams);
+		smallBoardDetector.setParams(smallBoardConfig, cameraParams);
 
 		while (videoCapture.grab()) {
 			//retrieve an image from the camera
 			videoCapture.retrieve(inputImage);
 
-			boardDetector.detect(inputImage);
+			Board board;
 
-			// detect the markers, and for each marker, determine pose and position
-			markers = boardDetector.getDetectedMarkers();
-			for (unsigned int i = 0; i < markers.size(); i++) {
-				Marker m = markers[i];
+			// Detect the images
+			if(smallBoardDetector.detect(inputImage) != -1) {
+				markers = smallBoardDetector.getDetectedMarkers();
+				board = smallBoardDetector.getDetectedBoard();
+			}
+			if(largeBoardDetector.detect(inputImage) != -1) {
+				markers = largeBoardDetector.getDetectedMarkers();
+				board = largeBoardDetector.getDetectedBoard();
+			}
 
-				//Display data to the meatbag humans
-//				for (int i = 0; i < 4; i++) {
-//					cout << "(" << m[i].x << "," << m[i].y << ") ";
-//					Point2f a(m[i].x, m[i].y);
-//				}
-//				cout << endl;
 
-				// brute force way to set up the camera matrix and parameters...may need to calibrate using ArUco
-				// if we do decide to use a calibrated parameter, then get ride of these lines...
-				// we read from a board configuration, so probably dont need to do this. hopefully
-				// cv::Mat CamMatrix = cv::Mat::eye(3, 3, CV_32F);
-				//CamMatrix.at<float>(0, 0) = 500;
-				//CamMatrix.at<float>(1, 1) = 500;
-				//CamMatrix.at<float>(0, 2) = inputImageCopy.size().width / 2;
-				//CamMatrix.at<float>(1, 2) = inputImageCopy.size().height / 2;
-				//cameraParams.setParams(CamMatrix, cv::Mat::zeros(1, 5, CV_32F), inputImageCopy.size());
+			// Determine pose and position based on the seen marker
+			if(markers.size() > 0) {
+				Marker m = markers[0];
+//			for (unsigned int i = 0; i < markers.size(); i++) {
+//				Marker m = markers[i];
 
 				vector <cv::Point3f> objPoints;
 				vector <cv::Point2f> imgPoints;
 
 				// get "3d" points of the object in real space, get "2d" points of the object in a flat image
-				getObjectAndImagePoints(boardDetector.getDetectedBoard(), objPoints, imgPoints);
+				getObjectAndImagePoints(board, objPoints, imgPoints);
 
 				// solve for transition matrix and store in rvec
 				// solve for rotation matrix and store in tvec
+				cv::Mat rvec, tvec;
 				solvePnP(objPoints, imgPoints, cameraParams.CameraMatrix, cameraParams.Distorsion, rvec, tvec);
 
 				// determine the position of the "camera" --> which in our case, will be the robot as well
@@ -156,13 +139,6 @@ int main(int argc,char **argv) {
 				cv::Mat cameraRotationVector;
 				cv::Rodrigues(R.t(), cameraRotationVector);
 				cv::Mat cameraTranslationVector = -R.t() * tvec;
-
-				// Don't print stuff to the screen, for SPEED.
-//				// print the translation vectors (distance between marker and camera in x, y, z direction)
-//				cout << "Camera position " << cameraTranslationVector.at<double>(0) << ", " << cameraTranslationVector.at<double>(1) << ", " << cameraTranslationVector.at<double>(2) << endl;
-//
-//				// print the pose vectors (orientation between marker and camera in x,y,z direction -- angle of rotation for each)
-//				cout << "Camera pose " << cameraRotationVector.at<double>(0) << ", " << cameraRotationVector.at<double>(1) << ", " << cameraRotationVector.at<double>(2) << endl;
 
 				// converting data from meters to centimeters
 				positionMsg.xDistance = cameraTranslationVector.at<double>(0) * 100.0;
@@ -197,7 +173,7 @@ int main(int argc,char **argv) {
 		return 0;
 	} // end try
 	catch (std::exception &ex) {
-		cout << "Exception :" << ex.what() << endl;
+		cout << "Position Sensoring Exception :" << ex.what() << endl;
 	} // end catch
 
 } // end main
--- start_node_scripts/run_camera_rotation.sh
+++ start_node_scripts/run_camera_rotation.sh
@@ -1,6 +1,7 @@
 source /home/pi/ROSta-Bot/devel/setup.bash
 
-boardConfig="/home/pi/ROSta-Bot/src/position_sensoring/985_meters.yml"
-cameraConfig="/home/pi/ROSta-Bot/src/position_sensoring/pseye_calibration.yml"
+largeBoardConfig="/home/pi/Pictures/985_meters.yml"
+smallBoardConfig="/home/pi/Pictures/smallMarker.yml"
+cameraConfig="/home/pi/Pictures/rocketfish_calibration.yml"
 
-rosrun position_sensoring position_sensoring_node $boardConfig $cameraConfig
+rosrun position_sensoring position_sensoring_node $largeBoardConfig $smallBoardConfig $cameraConfig
