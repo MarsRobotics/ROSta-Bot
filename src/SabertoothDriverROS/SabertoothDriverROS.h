8427e675b6c7802b03bfbfecebfd2e0fa11fabc0
Taylor Nightingale <nighting47@gmail.com>
2015-04-24 16:16:25 -0700

    Updated to be more clear

--- src/SabertoothDriverROS/SabertoothDriverROS.h
+++ src/SabertoothDriverROS/SabertoothDriverROS.h
@@ -17,17 +17,17 @@ const char FORWARD_LABEL = 1;
 const char BACKWARD_LABEL = 2;
 // Motor constants (used to communicate with the onboard computer).
 const unsigned char FRONT_LEFT_DRIVE_MOTOR_ID = 0;
-const unsigned char FRONT_RIGHT_DRIVE_MOTOR_ID = 1; // positive on purple
-const unsigned char MIDDLE_LEFT_DRIVE_MOTOR_ID = 2; 
-const unsigned char MIDDLE_RIGHT_DRIVE_MOTOR_ID = 3;
-const unsigned char REAR_LEFT_DRIVE_MOTOR_ID = 4; // Went clockwise. positive on white should be opposite articulation
+const unsigned char FRONT_RIGHT_DRIVE_MOTOR_ID = 1; 
+const unsigned char MIDDLE_LEFT_DRIVE_MOTOR_ID = 2;  // went clockwise. positive on purple
+const unsigned char MIDDLE_RIGHT_DRIVE_MOTOR_ID = 3; // went clockwise. positive on purple
+const unsigned char REAR_LEFT_DRIVE_MOTOR_ID = 4;    // Went clockwise. positive on white
 const unsigned char REAR_RIGHT_DRIVE_MOTOR_ID = 5;
 // Articulation motors
 const unsigned char FRONT_LEFT_ARTICULATION_MOTOR_ID = 6;
-const unsigned char FRONT_RIGHT_ARTICULATION_MOTOR_ID = 7;
-const unsigned char MIDDLE_LEFT_ARTICULATION_MOTOR_ID = 8; // went clockwise. positive on white
-const unsigned char MIDDLE_RIGHT_ARTICULATION_MOTOR_ID = 9;
-const unsigned char REAR_LEFT_ARTICULATION_MOTOR_ID = 10; // Went clockwise. positive on white
+const unsigned char FRONT_RIGHT_ARTICULATION_MOTOR_ID = 7; 
+const unsigned char MIDDLE_LEFT_ARTICULATION_MOTOR_ID = 8;  // went clockwise. 
+const unsigned char MIDDLE_RIGHT_ARTICULATION_MOTOR_ID = 9; // went clockwise. 
+const unsigned char REAR_LEFT_ARTICULATION_MOTOR_ID = 10;   // Went clockwise. 
 const unsigned char REAR_RIGHT_ARTICULATION_MOTOR_ID = 11;
 
 // Motor addressing (used to link the computer-constants to
