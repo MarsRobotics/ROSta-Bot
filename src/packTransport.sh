f38496cce0cb0913f45d264ee77ee45a9fad1919
Sherry liao <liao15@up.edu>
2015-05-07 21:18:44 -0700

    Added a way to wait between commands

--- src/packTransport.sh
+++ src/packTransport.sh
@@ -1,2 +1,3 @@
 rostopic pub ManualCommand command2ros/ManualCommand 90 90 90 90 90 90 0 0 0 0 0 0 0 false --once
+read -p "Press any key to continue... " -n1 -s
 rostopic pub ManualCommand command2ros/ManualCommand 0 180 0 180 0 180 0 0 0 0 0 0 0 false --once
