87cab156d694dbe6b6a9be1d68896d0f2d0d2177
Sherry liao <liao15@up.edu>
2015-05-07 21:12:09 -0700

    Add code to pack transport

---
+++ src/packTransport.sh
@@ -0,0 +1,2 @@
+rostopic pub ManualCommand command2ros/ManualCommand 90 90 90 90 90 90 0 0 0 0 0 0 0 false --once
+rostopic pub ManualCommand command2ros/ManualCommand 0 180 0 180 0 180 0 0 0 0 0 0 0 false --once
